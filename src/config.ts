f9bb3a54bd4faf88654e3bc66cfc8e94750754cc
curist <curist.cyc@gmail.com>
2018-09-23 23:08:41 +0800

    use main catworld url

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-const URL = 'wss://cw2.twmuds.com/websocket/v1939'
-// const URL = 'wss://catworld.muds.tw/websocket/v1939'
+// const URL = 'wss://cw2.twmuds.com/websocket/v1939'
+const URL = 'wss://catworld.muds.tw/websocket/v1939'
 // const URL = 'wss://doom.twmuds.com/websocket-doom/Doom'
 // const URL = 'wss://localhost:9999/what'
 // const URL = 'wss://windcloud.twmuds.com/websocket/'
